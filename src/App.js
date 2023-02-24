3739aa15ca24b0f84e98a3e004deb0194c51829b
Samuel Gouré <gmarcsamuel@gmail.com>
2023-02-24 11:58:27 +0000

    second formulaire in html, css

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div className='card'>
+          <div>
+            <p><img src="" alt="" /></p> 
+             <h1>How did we do?</h1>  
+             <p>Please let us know how we did with your support<br/> request. All feedback is appreciated to help us<br/> improve our offering!</p>
+          </div>  
+          <div className='button'>
+             <button className=''>1</button>
+             <button className=''>2</button>
+             <button className=''>3</button>
+             <button className=''>4</button>
+             <button className=''>5</button>
+          </div>    
+         <a href="#">SUBMIT</a>
+      
+       </div>
+    </div>
+  );
+}
+
+export default App;
