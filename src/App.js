2594a8ae76fd8f223a2852a1145b0478cc53b5fb
Samuel Gouré <gmarcsamuel@gmail.com>
2023-02-26 19:35:17 +0000

    ajout de couleur dans le card

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
        <div className='card'>
           <div>
-            <p><img src="" alt="" /></p> 
+            <p id='logo'> <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z" fill="#FC7614"/></svg>
+            </p> 
              <h1>How did we do?</h1>  
-             <p>Please let us know how we did with your support<br/> request. All feedback is appreciated to help us<br/> improve our offering!</p>
+             <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
           </div>  
           <div className='button'>
              <button className=''>1</button>
@@ -18,6 +19,7 @@ function App() {
              <button className=''>5</button>
           </div>    
          <a href="#">SUBMIT</a>
+         
       
        </div>
     </div>
